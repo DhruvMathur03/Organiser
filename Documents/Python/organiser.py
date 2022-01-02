a1267d743284e81e22a1ca457a3fcf832c1b0d56
DHRUV MATHUR <dhruvmathur@DHRUVs-MacBook-Air.local>
2022-01-01 19:04:17 -0500

    update

--- Documents/Python/organiser.py
+++ Documents/Python/organiser.py
@@ -1,16 +1,23 @@
 from pathlib import Path
 
-
+inp = input("Enter Path to be organised :")
 req_path = Path("/Users/dhruvmathur/Documents")
-main_path = Path("/Users/dhruvmathur")
-folders = ["Racket", "Python", "stuff", "PDF"]
+main_path = Path(inp)
+folders = ["Racket", "Python", "stuff", "PDF", "Roms", "apps"]
+
+def replacer(index, filename):
+    new_path = req_path.joinpath(folders[index])
+    new_file_path = new_path.joinpath(filename)
+    file.replace(new_file_path)
 
 for file in main_path.iterdir():
+
+    a = 0
+    ext = file.suffix
+    stem = file.stem
+    new_file = f'{file.stem}{ext}'
+
     if file.is_file() and file.stem != ".DS_Store":
-        a = 0
-        ext = file.suffix
-        stem = file.stem
-        new_file = f'{file.stem}{ext}'
 
         if ext == ".rkt":
             a = 0
@@ -18,14 +25,18 @@ for file in main_path.iterdir():
             a = 1
         elif ext == ".pdf":
             a = 3
+        elif ext == ".gba" or ext == ".nds":
+            a = 4
         else:
             a = 2
 
-        new_path = req_path.joinpath(folders[a])
-        new_file_path = new_path.joinpath(new_file)
-        file.replace(new_file_path)
+    elif ext == ".app":
+        a = 5
+
+    replacer(a, new_file)
 
         
+
 
 
 
