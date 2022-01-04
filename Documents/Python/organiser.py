875885ef460073c46a8ea3aa45d782d23c6ad2a2
DHRUV MATHUR <dhruvmathur@DHRUVs-MacBook-Air.local>
2022-01-03 19:55:26 -0500

    usability changes

--- Documents/Python/organiser.py
+++ Documents/Python/organiser.py
@@ -1,14 +1,42 @@
 from pathlib import Path
 
-inp = input("Enter Path to be organised :")
 req_path = Path("/Users/dhruvmathur/Documents")
-main_path = Path(inp)
+user_path = Path("/Users/dhruvmathur/")
 folders = ["Racket", "Python", "stuff", "PDF", "Roms", "apps", "CS-Assignments"]
+
+def paths(inp):
+
+    if inp == "Downloads":
+        req_user_path = user_path.joinpath("Downloads")
+        return req_user_path
+    elif inp == "Documents":
+        req_user_path = user_path.joinpath("Documents")
+        return req_user_path
+    elif inp == "Desktop":
+        req_user_path = user_path.joinpath("Desktop")
+        return req_user_path
+    
+
+def user_input():
+    options = ["Downloads", "Documents", "Desktop"]
+    for i in range(len(options)):
+        print(str(i+1) + ":" + options[i])
+    inp = int(input("Enter Number : "))
+    if inp in range(len(options) + 1):
+        inp = options[inp - 1]
+        return inp
+    else:
+        print("Invalid")
+        SystemExit
+    
 
 def replacer(index, filename):
     new_path = req_path.joinpath(folders[index])
     new_file_path = new_path.joinpath(filename)
     file.replace(new_file_path)
+
+a = user_input()
+main_path = paths(a)
 
 for file in main_path.iterdir():
 
